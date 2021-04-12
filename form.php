07a1a0a148ad19bf8939937edfcdb5915711296f
daanmeervan@gmail.com <Neo252004>
2021-04-12 13:14:59 +0200

    form

---
+++ form.php
@@ -0,0 +1,179 @@
+<form method="get" action="result.php">
+    <p>
+        Voor en achternaam
+        <input name="naam" value="" type="text">
+        <br>Geboortedatum
+        <input name="geboortedatum" value="" type="date">
+    </p>
+    <p>
+        <label>1. Reden van inschrijving?</label>
+    <ul>
+        <li>
+            <input type="radio" name="1" value="Ik vind het onderwerp interessant"> Ik vind het onderwerp interessant
+        </li>
+        <li>
+            <input type="radio" name="1" value="Ik vind gamen leuk"> Ik vind gamen leuk
+        </li>
+        <li>
+            <input type="radio" name="1" value="Ik moest iets kiezen"> Ik moest iets kiezen
+        </li>
+    </ul>
+    </p>
+    <p>
+        <label>2. Eerdere ervaring?</label>
+    <ul>
+        <li>
+            <input type="radio" name="2" value="Ja"> Ja
+        </li>
+        <li>
+            <input type="radio" name="2" value="Nee"> Nee
+        </li>
+    </ul>
+    </p>
+    <p>
+        <label>3. Kan je goed in een groepsverband werken?</label>
+    <ul>
+        <li>
+            <input type="radio" name="3" value="Ja"> Ja
+        </li>
+        <li>
+            <input type="radio" name="3" value="Nee"> Nee
+        </li>
+    </ul>
+    </p>
+    <p>
+        <label>4. Ken je mensen die deze opleiding volgen?</label>
+    <ul>
+        <li>
+            <input type="radio" name="4" value="Ja"> Ja
+        </li>
+        <li>
+            <input type="radio" name="4" value="Nee"> Nee
+        </li>
+    </ul>
+    </p>
+    <p>
+        <label>5. Wat ben je van plan te worden?</label>
+    <ul>
+        <li>
+            <input type="radio" name="5" value="Game designer"> Game designer
+        </li>
+        <li>
+            <input type="radio" name="5" value="Front end"> Front end
+        </li>
+        <li>
+            <input type="radio" name="5" value="Back end"> Back end
+        </li>
+    </ul>
+    </p>
+    <p>
+        <label>6. Ben je wel eens op een opendag geweest?</label>
+    <ul>
+        <li>
+            <input type="radio" name="6" value="Ja"> Ja
+        </li>
+        <li>
+            <input type="radio" name="6" value="Nee"> Nee
+        </li>
+    </ul>
+    </p>
+    <p>
+        <label>7. Heb je al iets gedaan rondom dit onderwerp?</label>
+    <ul>
+        <li>
+            <input type="radio" name="6" value="Ja"> Ja
+        </li>
+        <li>
+            <input type="radio" name="6" value="Nee"> Nee
+        </li>
+    </ul>
+    </p>
+    <p>
+        <label>8. Verwacht je een hacker hier mee te worden?</label>
+    <ul>
+        <li>
+            <input type="radio" name="6" value="Ja"> Ja
+        </li>
+        <li>
+            <input type="radio" name="6" value="Nee"> Nee
+        </li>
+    </ul>
+    </p>
+    <p>
+        <label>9. Kan je vragen stellen aan anderen?</label>
+    <ul>
+        <li>
+            <input type="radio" name="6" value="Ja"> Ja
+        </li>
+        <li>
+            <input type="radio" name="6" value="Nee"> Nee
+        </li>
+    </ul>
+    </p>
+    <p>
+        <label>10. Wat doe je met computer problemen?</label>
+    <ul>
+        <li>
+            <input type="radio" name="6" value="Uitzoeken wat het probleem is"> Uitzoeken wat het probleem is
+        </li>
+        <li>
+            <input type="radio" name="6" value="Start de laptop/computer opnieuw op"> Start de laptop/computer opnieuw op
+        </li>
+        <li>
+            <input type="radio" name="6" value="Laat hem repareren"> Laat hem repareren
+        </li>
+    </ul>
+    </p>
+    <p>
+        <label>11. Hoe vaak zit je achter je computer?</label>
+    <ul>
+        <li>
+            <input type="radio" name="6" value="0-4 uur"> 0-4 uur
+        </li>
+        <li>
+            <input type="radio" name="6" value="4-8 uur"> 4-8 uur
+        </li>
+        <li>
+            <input type="radio" name="6" value="8 uur of langer"> 8 uur of langer
+        </li>
+    </ul>
+    </p>
+    <p>
+        <label>12. Kan je goed omgaan met excel?</label>
+    <ul>
+        <li>
+            <input type="radio" name="6" value="Ja"> Ja
+        </li>
+        <li>
+            <input type="radio" name="6" value="Nee"> Nee
+        </li>
+    </ul>
+    </p>
+    <p>
+        <label>14. Kan je leren van je fouten?</label>
+    <ul>
+        <li>
+            <input type="radio" name="6" value="Ja"> Ja
+        </li>
+        <li>
+            <input type="radio" name="6" value="Nee"> Nee
+        </li>
+    </ul>
+    <p>
+        <label>15. Wat doe je als iemand kritiek geeft op je werk? </label>
+    <ul>
+        <li>
+            <input type="radio" name="6" value="Word boos"> Word boos
+        </li>
+        <li>
+            <input type="radio" name="6" value="Negeert"> Negeert
+        </li>
+        <li>
+            <input type="radio" name="6" value="Probeert het de volgende keer beter"> Probeert het de volgende keer beter
+        </li>
+    </ul>
+    </p>
+    <p>
+        <input type="submit" name="verzend" value="verzend">
+    </p>
+</form>
