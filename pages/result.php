c9e359106d938453eb3849fcb9e22c4da173a977
Administrator <Dion.kempe@edu-kw1c.nl>
2021-04-14 12:09:54 +0200

    result page

--- pages/result.php
+++ pages/result.php
@@ -4,34 +4,44 @@
     </title>
     <meta carset="UTF-8">
     <meta name="viewport" content="width=device-width, inital-scale=1.0">
+    <link rel='stylesheet' href='../css/result.css'>
     <link rel="stylesheet" href="../css/default.css">
+    <link rel='stylesheet' href='../css/result.css'>
     <script src="https://kit.fontawesome.com/c1698f882b.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 </head>
-<header>
-<?php
-    include '../includes/header.php';
-    ?>
-</header>
-<main>
-<?php
-//$calculation = $_GET["questionOne"]+$_GET["questionTwo"]+$_GET["questionThree"]+$_GET["questionFour"]+$_GET["questionFive"]+$_GET["questionSix"]+$_GET["questionSeven"]+$_GET["questionEight"]+$_GET["questionNine"]+$_GET["questionTen"]+$_GET["questionEleven"]+$_GET["questionTwelve"]+$_GET["questionThirteen"]+$_GET["questionFourteen"]+$_GET["questionFifteen"];
-$total = 0;
+<body>
+    <header>
+        <?php
+            include '../includes/header.php';
+        ?>
+    </header>
+    <main>
+        <?php
+            $total = 0;
 
-for($i=1;$i<15;$i++) {
-    $total += $_GET["question$i"];
-}
-$result = round($total, $precision = 0);
-if ($result >= 50) {
-    echo "Jou score is ".$result."%, de ICT opleiding is iets voor jou!";
-}
-else {
-    echo "Jou score is ".$result."%, de ICT opleiding is niet echt iets voor jou";
-}
-?>
-</main>
-<footer>
-<?php
-    include '../includes/footer.php';
-?>
-</footer>
+            for($i=1;$i<15;$i++) {
+                $total += $_GET["question$i"];
+            }
+
+            $result = round($total, $precision = 0);
+
+            echo "<h3>Gooie dag ".$_GET["name"]."</h3>";
+
+            if ($result >= 50) {
+                echo "Jou score is ".$result."%, de ICT opleiding is iets voor jou!";
+                echo "<br><img src='../images/check.png' alt='thumb'id='check'>";
+            }
+            else {
+                echo "Jou score is ".$result."%, de ICT opleiding is niet echt iets voor jou";
+                echo "<br><img src='../images/thumb_down.png' alt='thumb'id='thumb'>";
+            }
+        echo "<p> Uw opmerking &ldquo;".$_GET["remarks"]."&rdquo; is verzonden naar ons op ".date("j F, Y, g:i a")."</p>";
+        ?>
+    </main>
+    <footer>
+        <?php
+            include '../includes/footer.php';
+        ?>
+    </footer>
+</body>
