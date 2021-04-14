a8774d15a87e4cd8bb994ee2e56fd5d24036a07a
daanmeervan@gmail.com <Neo252004>
2021-04-14 13:12:28 +0200

    update

--- pages/form.php
+++ pages/form.php
@@ -28,180 +28,186 @@
         <br>
         <!-- Het opmaken van een form -->
         <form method="post" action="result.php">
-            <p>
-                Voor en achternaam
-                <input name="name" value="" type="text" required>
-                <br>Geboortedatum
-                <input name="data" value="" type="date" required>
-            </p>
-            <!--de titel van een vraag -->
-            <p>
-                <label>1. Reden van inschrijving?</label>
-            <ul>
-                <!-- antwoorden voor de bovenstaande vraag -->
-                <li>
-                    <input type="radio" name="question1" value=6.66 required> Ik vind het onderwerp interessant
-                </li>
-                <li>
-                    <input type="radio" name="question1" value=3.33 required> Ik vind gamen leuk
-                </li>
-                <li>
-                    <input type="radio" name="question1" value=0 required> Ik moest iets kiezen
-                </li>
-            </ul>
-            </p>
-            <!--de titel van een vraag -->
-            <p>
-                <label>2. Eerdere ervaring?</label>
-            <ul>
-                <!-- antwoorden voor de bovenstaande vraag -->
-                <li>
-                    <input type="radio" name="question2" value=6.66 required> Ja
-                </li>
-                <li>
-                    <input type="radio" name="question2" value=0 required> Nee
-                </li>
-            </ul>
-            </p>
-            <!--de titel van een vraag -->
-            <p>
-                <label>3. Kan je goed in een groepsverband werken?</label>
-            <ul>
-                <!-- antwoorden voor de bovenstaande vraag -->
-                <li>
-                    <input type="radio" name="question3" value=6.66 required> Ja
-                </li>
-                <li>
-                    <input type="radio" name="question3" value=0 required> Nee
-                </li>
-            </ul>
-            </p>
-            <!--de titel van een vraag -->
-            <p>
-                <label>4. Ken je mensen die deze opleiding volgen?</label>
-            <ul>
-                <!-- antwoorden voor de bovenstaande vraag -->
-                <li>
-                    <input type="radio" name="question4" value=6.66 required> Ja
-                </li>
-                <li>
-                    <input type="radio" name="question4" value=0 required> Nee
-                </li>
-            </ul>
-            </p>
-            <!--de titel van een vraag -->
-            <p>
-                <label>5. Wat ben je van plan te worden?</label>
-            <ul>
-                <!-- antwoorden voor de bovenstaande vraag -->
-                <li>
-                    <input type="radio" name="question5" value=6.66 required> Game designer
-                </li>
-                <li>
-                    <input type="radio" name="question5" value=6.66 required> Front end
-                </li>
-                <li>
-                    <input type="radio" name="question5" value=6.66> Back end
-                </li>
-            </ul>
-            </p>
-            <!--de titel van een vraag -->
-            <p>
-                <label>6. Ben je wel eens op een opendag geweest?</label>
-            <ul>
-                <!-- antwoorden voor de bovenstaande vraag -->
-                <li>
-                    <input type="radio" name="question6" value=6.66 required> Ja
-                </li>
-                <li>
-                    <input type="radio" name="question6" value=0 required> Nee
-                </li>
-            </ul>
-            </p>
-            <!--de titel van een vraag -->
-            <p>
-                <label>7. Heb je al iets gedaan rondom dit onderwerp?</label>
-            <ul>
-                <!-- antwoorden voor de bovenstaande vraag -->
-                <li>
-                    <input type="radio" name="question7" value=6.66 required> Ja
-                </li>
-                <li>
-                    <input type="radio" name="question7" value=0 required> Nee
-                </li>
-            </ul>
-            </p>
-            <!--de titel van een vraag -->
-            <p>
-                <label>8. Verwacht je een hacker hier mee te worden?</label>
-            <ul>
-                <!-- antwoorden voor de bovenstaande vraag -->
-                <li>
-                    <input type="radio" name="question8" value=6.66 required> Ja
-                </li>
-                <li>
-                    <input type="radio" name="question8" value=0 required> Nee
-                </li>
-            </ul>
-            </p>
-            <!--de titel van een vraag -->
-            <p>
-                <label>9. Kan je vragen stellen aan anderen?</label>
-            <ul>
-                <!-- antwoorden voor de bovenstaande vraag -->
-                <li>
-                    <input type="radio" name="question9" value=6.66 required> Ja
-                </li>
-                <li>
-                    <input type="radio" name="question9" value=0 required> Nee
-                </li>
-            </ul>
-            </p>
-            <!--de titel van een vraag -->
-            <p>
-                <label>10. Wat doe je met computer problemen?</label>
-            <ul>
-                <!-- antwoorden voor de bovenstaande vraag -->
-                <li>
-                    <input type="radio" name="question10" value=6.66 required> Uitzoeken wat het probleem is
-                </li>
-                <li>
-                    <input type="radio" name="question10" value=3.33 required> Start de laptop/computer opnieuw op
-                </li>
-                <li>
-                    <input type="radio" name="question10" value=0 required> Laat hem repareren
-                </li>
-            </ul>
-            </p>
-            <!--de titel van een vraag -->
-            <p>
-                <label>11. Hoe vaak zit je achter je computer?</label>
-            <ul>
-                <!-- antwoorden voor de bovenstaande vraag -->
-                <li>
-                    <input type="radio" name="question11" value=0 required> 0-4 uur
-                </li>
-                <li>
-                    <input type="radio" name="question11" value=3.33 required> 4-8 uur
-                </li>
-                <li>
-                    <input type="radio" name="question11" value=6.66 required> 8 uur of langer
-                </li>
-            </ul>
-            </p>
-            <!--de titel van een vraag -->
-            <p>
-                <label>12. Kan je goed omgaan met excel?</label>
-            <ul>
-                <!-- antwoorden voor de bovenstaande vraag -->
-                <li>
-                    <input type="radio" name="question12" value=6.66 required> Ja
-                </li>
-                <li>
-                    <input type="radio" name="question12" value=0 required> Nee
-                </li>
-            </ul>
-            </p>
+            <div id="links">
+                <p>
+                    Voor en achternaam
+                    <input name="name" value="" type="text" required>
+                    <br>Geboortedatum
+                    <input name="data" value="" type="date" required>
+                </p>
+                <!--de titel van een vraag -->
+                <p>
+                    <label>1. Reden van inschrijving?</label>
+                <ul>
+                    <!-- antwoorden voor de bovenstaande vraag -->
+                    <li>
+                        <input type="radio" name="question1" value=6.66 required> Ik vind het onderwerp interessant
+                    </li>
+                    <li>
+                        <input type="radio" name="question1" value=3.33 required> Ik vind gamen leuk
+                    </li>
+                    <li>
+                        <input type="radio" name="question1" value=0 required> Ik moest iets kiezen
+                    </li>
+                </ul>
+                </p>
+                <!--de titel van een vraag -->
+                <p>
+                    <label>2. Eerdere ervaring?</label>
+                <ul>
+                    <!-- antwoorden voor de bovenstaande vraag -->
+                    <li>
+                        <input type="radio" name="question2" value=6.66 required> Ja
+                    </li>
+                    <li>
+                        <input type="radio" name="question2" value=0 required> Nee
+                    </li>
+                </ul>
+                </p>
+                <!--de titel van een vraag -->
+                <p>
+                    <label>3. Kan je goed in een groepsverband werken?</label>
+                <ul>
+                    <!-- antwoorden voor de bovenstaande vraag -->
+                    <li>
+                        <input type="radio" name="question3" value=6.66 required> Ja
+                    </li>
+                    <li>
+                        <input type="radio" name="question3" value=0 required> Nee
+                    </li>
+                </ul>
+                </p>
+                <!--de titel van een vraag -->
+                <p>
+                    <label>4. Ken je mensen die deze opleiding volgen?</label>
+                <ul>
+                    <!-- antwoorden voor de bovenstaande vraag -->
+                    <li>
+                        <input type="radio" name="question4" value=6.66 required> Ja
+                    </li>
+                    <li>
+                        <input type="radio" name="question4" value=0 required> Nee
+                    </li>
+                </ul>
+                </p>
+            </div>
+            <div id="links2">
+                <!--de titel van een vraag -->
+                <p>
+                    <label>5. Wat ben je van plan te worden?</label>
+                <ul>
+                    <!-- antwoorden voor de bovenstaande vraag -->
+                    <li>
+                        <input type="radio" name="question5" value=6.66 required> Game designer
+                    </li>
+                    <li>
+                        <input type="radio" name="question5" value=6.66 required> Front end
+                    </li>
+                    <li>
+                        <input type="radio" name="question5" value=6.66> Back end
+                    </li>
+                </ul>
+                </p>
+                <!--de titel van een vraag -->
+                <p>
+                    <label>6. Ben je wel eens op een opendag geweest?</label>
+                <ul>
+                    <!-- antwoorden voor de bovenstaande vraag -->
+                    <li>
+                        <input type="radio" name="question6" value=6.66 required> Ja
+                    </li>
+                    <li>
+                        <input type="radio" name="question6" value=0 required> Nee
+                    </li>
+                </ul>
+                </p>
+                <!--de titel van een vraag -->
+                <p>
+                    <label>7. Heb je al iets gedaan rondom dit onderwerp?</label>
+                <ul>
+                    <!-- antwoorden voor de bovenstaande vraag -->
+                    <li>
+                        <input type="radio" name="question7" value=6.66 required> Ja
+                    </li>
+                    <li>
+                        <input type="radio" name="question7" value=0 required> Nee
+                    </li>
+                </ul>
+                </p>
+                <!--de titel van een vraag -->
+                <p>
+                    <label>8. Verwacht je een hacker hier mee te worden?</label>
+                <ul>
+                    <!-- antwoorden voor de bovenstaande vraag -->
+                    <li>
+                        <input type="radio" name="question8" value=6.66 required> Ja
+                    </li>
+                    <li>
+                        <input type="radio" name="question8" value=0 required> Nee
+                    </li>
+                </ul>
+                </p>
+            </div>
+            <div id="rechts">
+                <!--de titel van een vraag -->
+                <p>
+                    <label>9. Kan je vragen stellen aan anderen?</label>
+                <ul>
+                    <!-- antwoorden voor de bovenstaande vraag -->
+                    <li>
+                        <input type="radio" name="question9" value=6.66 required> Ja
+                    </li>
+                    <li>
+                        <input type="radio" name="question9" value=0 required> Nee
+                    </li>
+                </ul>
+                </p>
+                <!--de titel van een vraag -->
+                <p>
+                    <label>10. Wat doe je met computer problemen?</label>
+                <ul>
+                    <!-- antwoorden voor de bovenstaande vraag -->
+                    <li>
+                        <input type="radio" name="question10" value=6.66 required> Uitzoeken wat het probleem is
+                    </li>
+                    <li>
+                        <input type="radio" name="question10" value=3.33 required> Start de laptop/computer opnieuw op
+                    </li>
+                    <li>
+                        <input type="radio" name="question10" value=0 required> Laat hem repareren
+                    </li>
+                </ul>
+                </p>
+                <!--de titel van een vraag -->
+                <p>
+                    <label>11. Hoe vaak zit je achter je computer?</label>
+                <ul>
+                    <!-- antwoorden voor de bovenstaande vraag -->
+                    <li>
+                        <input type="radio" name="question11" value=0 required> 0-4 uur
+                    </li>
+                    <li>
+                        <input type="radio" name="question11" value=3.33 required> 4-8 uur
+                    </li>
+                    <li>
+                        <input type="radio" name="question11" value=6.66 required> 8 uur of langer
+                    </li>
+                </ul>
+                </p>
+                <!--de titel van een vraag -->
+                <p>
+                    <label>12. Kan je goed omgaan met excel?</label>
+                <ul>
+                    <!-- antwoorden voor de bovenstaande vraag -->
+                    <li>
+                        <input type="radio" name="question12" value=6.66 required> Ja
+                    </li>
+                    <li>
+                        <input type="radio" name="question12" value=0 required> Nee
+                    </li>
+                </ul>
+                </p>
+            </div>
             <!--de titel van een vraag -->
             <p>
                 <label>13. Kan je goed ontwerpen?</label>
