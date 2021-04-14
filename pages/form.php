a80dbb03a1711a46b8091d924d5e581ded43a764
daanmeervan@gmail.com <Neo252004>
2021-04-14 11:37:57 +0200

    commentaar form.css en form.php

--- pages/form.php
+++ pages/form.php
@@ -14,10 +14,19 @@
 <body>
     <header>
         <?php
+        // laad de header.php in deze pagina
         include "../includes/header.php";
         ?>
     </header>
+    <!-- Welkoms tekst voor de zelftest -->
     <main>
+        <p id="welkom">
+            Welkom bij de zelftest.<br>
+            Bij deze test kun je er achter komen of de ICT acedemie wel iets voor jouw is.<br>
+            Vul je vragen zo goed en eerlijk mogelijk in voor het beste resultaat.
+        </p>
+        <br>
+        <!-- Het opmaken van een form -->
         <form method="get" action="result.php">
             <p>
                 Voor en achternaam
@@ -25,9 +34,11 @@
                 <br>Geboortedatum
                 <input name="data" value="" type="date" required>
             </p>
+            <!--de titel van een vraag -->
             <p>
                 <label>1. Reden van inschrijving?</label>
             <ul>
+                <!-- antwoorden voor de bovenstaande vraag -->
                 <li>
                     <input type="radio" name="question1" value=6.66 required> Ik vind het onderwerp interessant
                 </li>
@@ -39,9 +50,11 @@
                 </li>
             </ul>
             </p>
+            <!--de titel van een vraag -->
             <p>
                 <label>2. Eerdere ervaring?</label>
             <ul>
+                <!-- antwoorden voor de bovenstaande vraag -->
                 <li>
                     <input type="radio" name="question2" value=6.66 required> Ja
                 </li>
@@ -50,9 +63,11 @@
                 </li>
             </ul>
             </p>
+            <!--de titel van een vraag -->
             <p>
                 <label>3. Kan je goed in een groepsverband werken?</label>
             <ul>
+                <!-- antwoorden voor de bovenstaande vraag -->
                 <li>
                     <input type="radio" name="question3" value=6.66 required> Ja
                 </li>
@@ -61,9 +76,11 @@
                 </li>
             </ul>
             </p>
+            <!--de titel van een vraag -->
             <p>
                 <label>4. Ken je mensen die deze opleiding volgen?</label>
             <ul>
+                <!-- antwoorden voor de bovenstaande vraag -->
                 <li>
                     <input type="radio" name="question4" value=6.66 required> Ja
                 </li>
@@ -72,9 +89,11 @@
                 </li>
             </ul>
             </p>
+            <!--de titel van een vraag -->
             <p>
                 <label>5. Wat ben je van plan te worden?</label>
             <ul>
+                <!-- antwoorden voor de bovenstaande vraag -->
                 <li>
                     <input type="radio" name="question5" value=6.66 required> Game designer
                 </li>
@@ -86,9 +105,11 @@
                 </li>
             </ul>
             </p>
+            <!--de titel van een vraag -->
             <p>
                 <label>6. Ben je wel eens op een opendag geweest?</label>
             <ul>
+                <!-- antwoorden voor de bovenstaande vraag -->
                 <li>
                     <input type="radio" name="question6" value=6.66 required> Ja
                 </li>
@@ -97,9 +118,11 @@
                 </li>
             </ul>
             </p>
+            <!--de titel van een vraag -->
             <p>
                 <label>7. Heb je al iets gedaan rondom dit onderwerp?</label>
             <ul>
+                <!-- antwoorden voor de bovenstaande vraag -->
                 <li>
                     <input type="radio" name="question7" value=6.66 required> Ja
                 </li>
@@ -108,9 +131,11 @@
                 </li>
             </ul>
             </p>
+            <!--de titel van een vraag -->
             <p>
                 <label>8. Verwacht je een hacker hier mee te worden?</label>
             <ul>
+                <!-- antwoorden voor de bovenstaande vraag -->
                 <li>
                     <input type="radio" name="question8" value=6.66 required> Ja
                 </li>
@@ -119,9 +144,11 @@
                 </li>
             </ul>
             </p>
+            <!--de titel van een vraag -->
             <p>
                 <label>9. Kan je vragen stellen aan anderen?</label>
             <ul>
+                <!-- antwoorden voor de bovenstaande vraag -->
                 <li>
                     <input type="radio" name="question9" value=6.66 required> Ja
                 </li>
@@ -130,9 +157,11 @@
                 </li>
             </ul>
             </p>
+            <!--de titel van een vraag -->
             <p>
                 <label>10. Wat doe je met computer problemen?</label>
             <ul>
+                <!-- antwoorden voor de bovenstaande vraag -->
                 <li>
                     <input type="radio" name="question10" value=6.66 required> Uitzoeken wat het probleem is
                 </li>
@@ -144,9 +173,11 @@
                 </li>
             </ul>
             </p>
+            <!--de titel van een vraag -->
             <p>
                 <label>11. Hoe vaak zit je achter je computer?</label>
             <ul>
+                <!-- antwoorden voor de bovenstaande vraag -->
                 <li>
                     <input type="radio" name="question11" value=0 required> 0-4 uur
                 </li>
@@ -158,9 +189,11 @@
                 </li>
             </ul>
             </p>
+            <!--de titel van een vraag -->
             <p>
                 <label>12. Kan je goed omgaan met excel?</label>
             <ul>
+                <!-- antwoorden voor de bovenstaande vraag -->
                 <li>
                     <input type="radio" name="question12" value=6.66 required> Ja
                 </li>
@@ -169,9 +202,11 @@
                 </li>
             </ul>
             </p>
+            <!--de titel van een vraag -->
             <p>
                 <label>13. Kan je goed ontwerpen?</label>
             <ul>
+                <!-- antwoorden voor de bovenstaande vraag -->
                 <li>
                     <input type="radio" name="question13" value=6.66 required> Ja
                 </li>
@@ -180,9 +215,11 @@
                 </li>
             </ul>
             </p>
+            <!--de titel van een vraag -->
             <p>
                 <label>14. Kan je leren van je fouten?</label>
             <ul>
+                <!-- antwoorden voor de bovenstaande vraag -->
                 <li>
                     <input type="radio" name="question14" value=6.66 required> Ja
                 </li>
@@ -190,9 +227,12 @@
                     <input type="radio" name="question14" value=0 required> Nee
                 </li>
             </ul>
+            </p>
+            <!--de titel van een vraag -->
             <p>
                 <label>15. Wat doe je als iemand kritiek geeft op je werk? </label>
             <ul>
+                <!-- antwoorden voor de bovenstaande vraag -->
                 <li>
                     <input type="radio" name="question15" value=0 required> Word boos
                 </li>
@@ -204,10 +244,12 @@
                 </li>
             </ul>
             </p>
+            <!--een plek waar opmerkingen kunnen worden ingezet -->
             <p>
                 <label>Opmerkingen<br> </label>
                 <textarea name="remarks"></textarea>
             </p>
+            <!-- de verzend knop -->
             <p>
                 <input type="submit" name="send" value="verzend">
             </p>
@@ -216,6 +258,7 @@
         <br>
         <br>
     </main>
+    <!-- laad de footer.php pagina in -->
     <footer>
         <?php
         include "../includes/footer.php";
