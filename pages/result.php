b076e905b7b80159986e162ce3372ae283f42697
Microwave4614 <aanvalshelikopter@gmail.com>
2021-04-14 13:19:33 +0200

    meneer van meer kan geen background-color maken

--- pages/result.php
+++ pages/result.php
@@ -15,12 +15,10 @@ aanmaakdatum: <9-4-2020>
         <meta carset="UTF-8">
         <meta name="viewport" content="width=device-width, inital-scale=1.0">
         <link rel='stylesheet' href='../css/result.css'>
-        <link rel="stylesheet" href="../css/default.css">
-        <link rel='stylesheet' href='../css/result.css'>
         <script src="https://kit.fontawesome.com/c1698f882b.js" crossorigin="anonymous"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     </head>
-    <body>
+    <body id="achtergrond">
     <!--Header include-->
         <header>
             <?php
