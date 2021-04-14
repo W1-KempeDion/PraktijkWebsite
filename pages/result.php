27b3987374cde589f1d0ea51c3898efae818aed1
Administrator <Dion.kempe@edu-kw1c.nl>
2021-04-14 12:19:02 +0200

    result page

--- pages/result.php
+++ pages/result.php
@@ -1,47 +1,68 @@
-<head>
-    <title>
-        Zelftest - Koning Willem 1 College
-    </title>
-    <meta carset="UTF-8">
-    <meta name="viewport" content="width=device-width, inital-scale=1.0">
-    <link rel='stylesheet' href='../css/result.css'>
-    <link rel="stylesheet" href="../css/default.css">
-    <link rel='stylesheet' href='../css/result.css'>
-    <script src="https://kit.fontawesome.com/c1698f882b.js" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-</head>
-<body>
-    <header>
-        <?php
-            include '../includes/header.php';
-        ?>
-    </header>
-    <main>
-        <?php
-            $total = 0;
+<!DOCTYPE html>
+<!--
+Auteur:     <Dion Kempe>
+aanmaakdatum: <9-4-2020>
 
-            for($i=1;$i<15;$i++) {
-                $total += $_GET["question$i"];
-            }
+<De result page van de form>
+-->
+<html>
+    <head>
+    <!--    Titel van de pagina-->
+        <title>
+            Zelftest - Koning Willem 1 College
+        </title>
+    <!--    Meta's, Links naar css en scripts-->
+        <meta carset="UTF-8">
+        <meta name="viewport" content="width=device-width, inital-scale=1.0">
+        <link rel='stylesheet' href='../css/result.css'>
+        <link rel="stylesheet" href="../css/default.css">
+        <link rel='stylesheet' href='../css/result.css'>
+        <script src="https://kit.fontawesome.com/c1698f882b.js" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    </head>
+    <body>
+    <!--Header include-->
+        <header>
+            <?php
+                include '../includes/header.php';
+            ?>
+        </header>
+    <!--main stuk van de pagina-->
+        <main>
+            <?php
+    //        variables
+                $total = 0;
 
-            $result = round($total, $precision = 0);
+    //            forloop die de score uitrekent
+                for($i=1;$i<15;$i++) {
+                    $total += $_GET["question$i"];
+                }
 
-            echo "<h3>Gooie dag ".$_GET["name"]."</h3>";
+    //            afronden van de score
+                $result = round($total, $precision = 0);
 
-            if ($result >= 50) {
-                echo "Jou score is ".$result."%, de ICT opleiding is iets voor jou!";
-                echo "<br><img src='../images/check.png' alt='thumb'id='check'>";
-            }
-            else {
-                echo "Jou score is ".$result."%, de ICT opleiding is niet echt iets voor jou";
-                echo "<br><img src='../images/thumb_down.png' alt='thumb'id='thumb'>";
-            }
-        echo "<p> Uw opmerking &ldquo;".$_GET["remarks"]."&rdquo; is verzonden naar ons op ".date("j F, Y, g:i a")."</p>";
-        ?>
-    </main>
-    <footer>
-        <?php
-            include '../includes/footer.php';
-        ?>
-    </footer>
-</body>
+    //            welkom bericht
+                echo "<h3>Gooie dag ".$_GET["name"]."</h3>";
+
+    //            if/else statment voor het uitvinden van een positive of negative score
+                if ($result >= 50) {
+                    echo "Jou score is ".$result."%, de ICT opleiding is iets voor jou!";
+                    echo "<br><img src='../images/check.png' alt='thumb'id='check'>";
+                }
+                else {
+                    echo "Jou score is ".$result."%, de ICT opleiding is niet echt iets voor jou";
+                    echo "<br><img src='../images/thumb_down.png' alt='thumb'id='thumb'>";
+                }
+
+    //            Opmerking met datum
+            echo "<p> Uw opmerking &ldquo;".$_GET["remarks"]."&rdquo; is verzonden naar ons op ".date("j F, Y, g:i a")."</p>";
+            ?>
+        </main>
+    <!--Include van de footer-->
+        <footer>
+            <?php
+                include '../includes/footer.php';
+            ?>
+        </footer>
+    </body>
+</html>
